f0807bc98932a58242e5ea20adb6c25be7c8d864
bagus-ganteng <61874925+bagus-ganteng@users.noreply.github.com>
2020-05-07 19:52:00 +0700

    Upload Repos

---
+++ Tugas Poly/Tugas Poly/PrinterWindows.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Poly
+{
+    public class PrinterWindows
+    {
+        //properties
+        public string Name { get; set; }
+
+        //method
+        public virtual void Show()
+        {
+            Console.WriteLine("Display dimension");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Printing ...");
+        }
+
+        public void Jeneng()
+        {
+            Console.WriteLine("\n\nMohamad Bagus Rizky \nIF02 \n19.11.2711");
+        }
+    }
+
+
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing ...");
+        }
+    }
+    public class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing ...");
+        }
+    }
+    public class Lasetjet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LasetJet display dimension : 12*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LasetJet printer printing ...");
+        }
+    }
+
+}
---
+++ Tugas Poly/Tugas Poly/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tugas_Poly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih Printer: ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LasetJet\n");
+
+            Console.WriteLine("Nomor Printer [1...3]:");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else
+                printer = new Lasetjet();
+
+            printer.Show();
+            printer.Print();
+            printer.Jeneng();
+
+            Console.ReadKey();
+
+        }
+    }
+}
